1ec7cdc55bf42239093cc599232e49966b5dcb6f
Enginland <maosomith.kh@gmail.com>
2024-10-30 23:15:00 +0700

    modify

--- apps/customer/models.py
+++ apps/customer/models.py
@@ -13,21 +13,3 @@ class Customer(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
-
-# from django.db import models
-
-# class Supplier(models.Model):
-#     name = models.CharField(max_length=255)
-#     contact_name = models.CharField(max_length=255, null=True, blank=True)
-#     phone_number = models.CharField(max_length=20, null=True, blank=True)
-#     email = models.EmailField(null=True, blank=True)
-#     address = models.TextField(null=True, blank=True)
-#     website = models.URLField(null=True, blank=True)
-#     supplier_code = models.CharField(max_length=100, unique=True, null=True, blank=True)
-#     tax_id = models.CharField(max_length=50, null=True, blank=True)
-#     payment_terms = models.CharField(max_length=100, null=True, blank=True)
-#     notes = models.TextField(null=True, blank=True)
-#     date_added = models.DateTimeField(auto_now_add=True)
-
-#     def __str__(self):
-#         return self.name
--- apps/customer/urls.py
+++ apps/customer/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('customers/', views.customer_list, name='customer_list'),
+    path('', views.customer_list, name='customer_list'),
     path('customers/new/', views.customer_create, name='customer_create'),
     path('customers/edit/<int:id>/', views.customer_update, name='customer_update'),
      path('customers/delete/<int:id>/', views.customer_delete, name='customer_delete'),  # Delete a supplier
