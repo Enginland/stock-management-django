ca9d7f5c71c817bf71b220c908583731cf9bd6e5
Enginland <maosomith.kh@gmail.com>
2024-10-30 20:14:29 +0700

    modify

--- apps/customer/urls.py
+++ apps/customer/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import customer_list, customer_create, customer_update, customer_delete
+from . import views
 
 urlpatterns = [
-    path('', customer_list, name='customer_list'),  # List all customers
-    # path('<int:customer_id>/', customer_detail, name='customer_detail'),  # View customer details
-    path('create/', customer_create, name='customer_create'),  # Create new customer
-    path('update/<int:id_id>/', customer_update, name='customer_update'),  # Update customer
-    path('customer/<int:id_id>/delete/', customer_delete, name='customer_delete'),
+    path('customers/', views.customer_list, name='customer_list'),
+    path('customers/new/', views.customer_create, name='customer_create'),
+    path('customers/edit/<int:id>/', views.customer_update, name='customer_update'),
+     path('customers/delete/<int:id>/', views.customer_delete, name='customer_delete'),  # Delete a supplier
 ]
