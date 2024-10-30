f996fee322ef064e199525db23471c50091b090b
Enginland <maosomith.kh@gmail.com>
2024-10-30 20:15:04 +0700

    modify

--- apps/customer/views.py
+++ apps/customer/views.py
@@ -1,40 +1,39 @@
+from pyexpat.errors import messages
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib import messages
 from .models import Customer
 from .forms import CustomerForm
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-# List Customers
+
+# List Suppliers
 def customer_list(request):
-    search_term = request.GET.get('search', '')  # Get the search term from the query parameters
-    customer = Customer.objects.all()
+    customers = Customer.objects.all()
+    return render(request, 'customer/customer_list.html', {'customers': customers})
 
-    # Filter stocks based on the search term
-    if search_term:
-        customer = customer.filter(name__icontains=search_term)
-
-    paginator = Paginator(customer, 20)  # Show 10 stocks per page
-    page_number = request.GET.get('page')
-
-    try:
-        customer_page = paginator.get_page(page_number)
-    except PageNotAnInteger:
-        customer_page = paginator.page(1)  # If page is not an integer, deliver first page.
-    except EmptyPage:
-        customer_page = paginator.page(paginator.num_pages)  # If page is out of range, deliver last page of results.
-
-    return render(request, 'customer/customer_list.html', {
-        'customer': customer_page,
-        'search_term': search_term,  # Pass the search term to the template
-    })
-# Add or Edit Customer
+# Add or Edit Supplier
 def customer_create(request, id=None):
     if id:
-        customer = get_object_or_404(Customer, id=id)
+        customers = get_object_or_404(Customer, id=id)
     else:
-        customer = None
+        customers = None
 
     if request.method == 'POST':
-        form = CustomerForm(request.POST, instance=customer)
+        form = CustomerForm(request.POST, instance=customers)
+        if form.is_valid():
+            form.save()
+            return redirect('customer_list')
+    else:
+        form = CustomerForm(instance=customers)
+    
+    return render(request, 'customer/customer_list.html', {'form': form})
+
+
+def customer_update(request, id=None):
+    if id:
+        customers = get_object_or_404(Customer, id=id)  # If ID is provided, update an existing supplier
+    else:
+        customers = None  # Otherwise, create a new supplier
+
+    if request.method == 'POST':
+        form = CustomerForm(request.POST, instance=customers)
         if form.is_valid():
             form.save()
             if id:
@@ -43,35 +42,17 @@ def customer_create(request, id=None):
                 messages.success(request, 'Customer added successfully!')
             return redirect('customer_list')
     else:
-        form = CustomerForm(instance=customer)
-    
-    return render(request, 'customer/customer_list.html', {'form': form})
-
-# Update Customer
-def customer_update(request, id=None):
-    if id:
-        customer = get_object_or_404(Customer, id=id)
-    else:
-        customer = None
-
-    if request.method == 'POST':
-        form = CustomerForm(request.POST, instance=customer)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Customer updated successfully!' if id else 'Customer added successfully!')
-            return redirect('customer_list')
-    else:
-        form = CustomerForm(instance=customer)
+        form = CustomerForm(instance=customers)
 
     return render(request, 'customer/customer_list.html', {'form': form})
 
-# Delete Customer
+# Delete Supplier (DELETE)
 def customer_delete(request, id):
-    customer = get_object_or_404(Customer, id=id)
+    customers = get_object_or_404(Customer, id=id)
 
     if request.method == 'POST':
-        customer.delete()
-        messages.success(request, 'Customer deleted successfully!')
+        customers.delete()
+        messages.success(request, 'Customers deleted successfully!')
         return redirect('customer_list')
 
-    return render(request, 'customer/customer_list.html', {'customer': customer})
+    return render(request, 'customer/customer_list.html', {'supplier': customers})
