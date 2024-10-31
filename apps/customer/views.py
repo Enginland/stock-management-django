c613c27fb94106dfacfbfa2c8dc5aa6bc21c9938
Enginland <maosomith.kh@gmail.com>
2024-10-31 20:56:02 +0700

    modify

--- apps/customer/views.py
+++ apps/customer/views.py
@@ -1,58 +1,65 @@
-from pyexpat.errors import messages
 from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
 from .models import Customer
-from .forms import CustomerForm
+from .forms import CustomerForm  # You will create this form next
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-# List Suppliers
+# List all stocks with pagination and search
 def customer_list(request):
+    search_term = request.GET.get('search', '')  # Get the search term from the query parameters
     customers = Customer.objects.all()
-    return render(request, 'customer/customer_list.html', {'customers': customers})
 
-# Add or Edit Supplier
-def customer_create(request, id=None):
-    if id:
-        customers = get_object_or_404(Customer, id=id)
-    else:
-        customers = None
+    # Filter stocks based on the search term
+    if search_term:
+        customers = customers.filter(name__icontains=search_term)
 
+    paginator = Paginator(customers, 20)  # Show 10 stocks per page
+    page_number = request.GET.get('page')
+
+    try:
+        customers_page = paginator.get_page(page_number)
+    except PageNotAnInteger:
+        customers_page = paginator.page(1)  # If page is not an integer, deliver first page.
+    except EmptyPage:
+        customers_page = paginator.page(paginator.num_pages)  # If page is out of range, deliver last page of results.
+
+    return render(request, 'customer/customer_list.html', {
+        'customers': customers_page,
+        'search_term': search_term,  # Pass the search term to the template
+    })
+
+# View stock detail
+def customer_detail(request, pk):
+    customer = get_object_or_404(Customer, pk=pk)
+    return render(request, 'customer/customer_detail.html', {'customer': customer})
+
+# Create a new stock
+def customer_create(request):
     if request.method == 'POST':
-        form = CustomerForm(request.POST, instance=customers)
+        form = CustomerForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('customer_list')
+            return redirect('customer_list')  # Redirect to stock list after creation
     else:
-        form = CustomerForm(instance=customers)
-    
-    return render(request, 'customer/customer_list.html', {'form': form})
+        form = CustomerForm()
+    # return render(request, 'stock/stock_create.html', {'form': form})
 
-
-def customer_update(request, id=None):
-    if id:
-        customers = get_object_or_404(Customer, id=id)  # If ID is provided, update an existing supplier
-    else:
-        customers = None  # Otherwise, create a new supplier
-
+# Update an existing stock
+def customer_update(request, pk):
+    customer = get_object_or_404(Customer, pk=pk)
     if request.method == 'POST':
-        form = CustomerForm(request.POST, instance=customers)
+        form = CustomerForm(request.POST, instance=customer)
         if form.is_valid():
             form.save()
-            if id:
-                messages.success(request, 'Customer updated successfully!')
-            else:
-                messages.success(request, 'Customer added successfully!')
-            return redirect('customer_list')
+            return redirect('customer_list', pk=customer.pk)
     else:
-        form = CustomerForm(instance=customers)
+        form = CustomerForm(instance=customer)
+    # return render(request, 'customer_list', {'form': form})
 
-    return render(request, 'customer/customer_list.html', {'form': form})
-
-# Delete Supplier (DELETE)
-def customer_delete(request, id):
-    customers = get_object_or_404(Customer, id=id)
-
+# Delete an existing stock
+def customer_delete(request, pk):
+    customer = get_object_or_404(Customer, pk=pk)
     if request.method == 'POST':
-        customers.delete()
-        messages.success(request, 'Customers deleted successfully!')
+        customer.delete()
         return redirect('customer_list')
-
-    return render(request, 'customer/customer_list.html', {'supplier': customers})
+    # return render(request, 'stock/stock_delete.html', {'stock': stock})
