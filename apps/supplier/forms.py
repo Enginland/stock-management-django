f076c08a8a61c5029012b111947e856526da81dc
Enginland <maosomith.kh@gmail.com>
2024-11-01 17:45:55 +0700

    modify

--- apps/supplier/forms.py
+++ apps/supplier/forms.py
@@ -5,3 +5,6 @@ class SupplierForm(forms.ModelForm):
     class Meta:
         model = Supplier
         fields = ['name', 'contact_name', 'phone_number', 'email', 'address', 'website', 'supplier_code', 'tax_id', 'payment_terms', 'notes']
+        widgets = {
+            'website': forms.URLInput(attrs={'placeholder': 'Optional'}),  # Use URLInput instead of WebsiteInput
+        }
