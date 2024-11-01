32fe005d94c888726941650dc998b2cfefe7860d
Enginland <maosomith.kh@gmail.com>
2024-11-01 17:46:50 +0700

    modify

--- apps/supplier/urls.py
+++ apps/supplier/urls.py
@@ -1,9 +1,11 @@
+
 from django.urls import path
-from . import views
+from .views import supplier_list, supplier_detail, supplier_create, supplier_update, supplier_delete
 
 urlpatterns = [
-    path('suppliers/', views.supplier_list, name='supplier_list'),
-    path('suppliers/new/', views.supplier_create, name='supplier_create'),
-    path('suppliers/edit/<int:id>/', views.supplier_update, name='supplier_update'),
-     path('suppliers/delete/<int:id>/', views.supplier_delete, name='supplier_delete'),  # Delete a supplier
+    path('', supplier_list, name='supplier_list'),
+    path('supplier/<int:pk>/', supplier_detail, name='supplier_detail'),
+    path('supplier/create/', supplier_create, name='supplier_create'),
+    path('supplier/<int:pk>/update/', supplier_update, name='supplier_update'),
+    path('supplier/<int:pk>/delete/', supplier_delete, name='supplier_delete'),
 ]
