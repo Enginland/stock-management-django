67a9d595a7c2b1edd56e1174d7360373126ebbae
Enginland <maosomith.kh@gmail.com>
2024-11-02 19:50:53 +0700

    modify

--- apps/stock/urls.py
+++ apps/stock/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from main import views  # Make sure you have the correct views imported
 
 from django.urls import path
-from .views import stock_list, stock_detail, stock_create, stock_update, stock_delete
+from .views import generate_pdf_report, stock_list, stock_detail, stock_create, stock_update, stock_delete
 
 urlpatterns = [
     path('', stock_list, name='stock_list'),
@@ -12,4 +12,6 @@ urlpatterns = [
     path('stock/create/', stock_create, name='stock_create'),
     path('stock/<int:pk>/update/', stock_update, name='stock_update'),
     path('stock/<int:pk>/delete/', stock_delete, name='stock_delete'),
+    path('reports/stock/pdf/', generate_pdf_report, name='generate_pdf_report'),
+
 ]
